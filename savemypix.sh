ceeb62e4da2606905888a9f31bfac86a1ddd24fd
Ryan McGinnis <mcginnisrr@gmail.com>
2016-06-20 20:52:36 -0700

    Update savemypix.sh

--- savemypix.sh
+++ savemypix.sh
@@ -9,7 +9,7 @@ pext='.photoslibrary'
 if [ $# -eq 0 ]; then
 	des=$(df -lh | awk '{print $9}' | tail -n 1)
 	src=("$HOME/Pictures/Photos Library"$pext"/Masters/")
-	if [ -f "$HOME/Pictures/Photos Library.photoslibrary" ]; then
+	if [ -e "$HOME/Pictures/Photos Library.photoslibrary" ]; then
 		mkdir $des/PhotosBackup$bt
 		rsync -aPv "$src" "$des/PhotosBackup$bt/"
 	else
