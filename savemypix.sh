f711a44ee8c07d4389374f2573147d055c2aacf8
Ryan McGinnis <mcginnisrr@gmail.com>
2016-06-20 20:45:32 -0700

    Update savemypix.sh

--- savemypix.sh
+++ savemypix.sh
@@ -4,13 +4,22 @@
 # Apple Store San Francisco (R075)
 
 bt=$(date "+%m_%d_%Y")
+pext='.photoslibrary'
 
 if [ $# -eq 0 ]; then
 	des=$(df -lh | awk '{print $9}' | tail -n 1)
-	src=("$HOME/Pictures/iPhoto Library"*"/Masters/")
-	mkdir $des/iPhotoBackup$bt
-	sudo rsync -aPv "$src" "$des/iPhotoBackup$bt/"
+	src=("$HOME/Pictures/Photos Library"$pext"/Masters/")
+	if [ ! -f "$HOME/Pictures/Photos Library.photoslibrary" ]; then
+		mkdir $des/PhotosBackup$bt
+		rsync -aPv "$src" "$des/PhotosBackup$bt/"
+	elif [ ! -f "$HOME/Pictures/iPhoto Library.iphotolibrary" ]; then
+		mkdir $des/iPhotoBackup$bt
+		rsync -aPv "$src" "$des/iPhotoBackup$bt/"
+	else
+		echo "No Photos or iPhoto Library, or its filename is not default."
+		exit
+	fi
 else	
-	mkdir $2/iPhotoBackup$bt
-	sudo rsync -aPv $1/ $2/iPhotoBackup$bt/
+	mkdir $2/PhotosBackup$bt
+	rsync -aPv $1/ $2/PhotosBackup$bt/
 fi
